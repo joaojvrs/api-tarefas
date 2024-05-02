7ee9a58e9da749420745f3f1daba33480336d090
joaojvrs <168686160+joaojvrs@users.noreply.github.com>
2024-05-01 23:01:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var userArgs = process.argv.slice(2);
 var mongoURL = userArgs[0];
 //Configurando a conexao com o Banco de Dados
 var mongoose = require('mongoose');
-mongoose.connect(mongoURL);
+mongoose.connect("mongodb+srv://joao226747:CLPxUPofiiSWzPxa@joao226747.zgoxkyz.mongodb.net/tarefasDB");
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', (error) => {
